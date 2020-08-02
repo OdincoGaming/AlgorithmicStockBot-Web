77d92ecfc8a8df3496e1c214e9b9da2656935188
Freddy Lyle <OdincoGaming@gmail.com>
2020-08-02 14:21:59 -0500

    condense server.js

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
