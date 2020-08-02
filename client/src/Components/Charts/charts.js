420a7f4baae9bfdecc6935d36bdb5f040032908c
Freddy Lyle <OdincoGaming@gmail.com>
2020-08-02 14:16:39 -0500

    removed whitespace on charts

--- client/src/Components/Charts/charts.js
+++ client/src/Components/Charts/charts.js
@@ -39,9 +39,7 @@ render() {
                         right: 0,
                         top: 0,
                         bottom: 0
-                      },
-                      
-                    
+                      },   
                 }}
               />
             </div>
